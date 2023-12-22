fd61587140ed200bdb33672af2664cf0278c664e
BraveRam <99811385+BraveRam@users.noreply.github.com>
2023-12-22 14:15:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
+from OpsAi import Ai
 
 app = Flask(__name__)
 
 @app.route('/ask', methods=['POST'])
 def receive_message():
     data = request.get_json()
-    received_message = data.get('message', '')    
-    response_data = {'status': 'success', 'message': 'Message received successfully'}
+    received_message = data.get('message', '')
+    req = Ai(query=received_message)
+    res = req.chat()
+    response_data = {'status': 'success', 'message': res}
     return jsonify(response_data)
